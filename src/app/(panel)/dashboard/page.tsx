8da918aa6891ed964a4b84cbfe53eaffdcdb4171
Victor-Watanabe <watavh@hotmail.com>
2026-01-05 19:34:18 -0300

    OP - 07 Desenvolvendo o Sidebar mobile

--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -3,7 +3,12 @@ import { Header } from "@/app/(public)/_components/Header";
 export default function Dashboard(){
     return(
         <div>
-            <Header></Header>
+            <h1>Página Dashboard</h1>
+        <div className="w-full h-150 bg-gray-300 mb-10"></div>
+        
+        <div className="w-full h-150 bg-gray-700 mb-10"></div>
+
+        <div className="w-full h-150 bg-gray-100 mb-10"></div>
         </div>
     )
 }
