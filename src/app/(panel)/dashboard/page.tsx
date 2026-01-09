8f12af4e98a6c9795da609277acff143f80de144
Victor-Watanabe <watavh@hotmail.com>
2026-01-08 22:18:31 -0300

    OP - 13 Pequenos ajustes no SideBar e coletando dados do usuário da sessão

--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -1,14 +1,22 @@
-import { Header } from "@/app/(public)/_components/Header";
+import getSession from "@/lib/getSession"
+import { redirect } from "next/navigation"
 
-export default function Dashboard(){
+export default async function Dashboard(){
+    const session = await getSession()
+
+    if(!session) {
+        redirect('/')
+    }
+
     return(
         <div>
             <h1>Página Dashboard</h1>
-        <div className="w-full h-150 bg-gray-300 mb-10"></div>
-        
-        <div className="w-full h-150 bg-gray-700 mb-10"></div>
 
-        <div className="w-full h-150 bg-gray-100 mb-10"></div>
+            <div className="w-full h-150 bg-gray-300 mb-10"></div>
+            
+            <div className="w-full h-150 bg-gray-700 mb-10"></div>
+
+            <div className="w-full h-150 bg-gray-100 mb-10"></div>
         </div>
     )
 }
