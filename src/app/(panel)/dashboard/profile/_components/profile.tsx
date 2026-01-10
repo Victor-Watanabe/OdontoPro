e28a94498684fd6a1b67d344c4fffe517dd6892c
Victor-Watanabe <watavh@hotmail.com>
2026-01-09 23:11:15 -0300

    OP - 16 Correção de horário disponível da clínica.

--- src/app/(panel)/dashboard/profile/_components/profile.tsx
+++ src/app/(panel)/dashboard/profile/_components/profile.tsx
@@ -23,7 +23,7 @@ export function ProfileContent(){
     function generateTimeSlots() : string[]{
         const hours : string[] = [];
 
-        for (let i = 8; i<= 24; i++) {
+        for (let i = 8; i<= 23; i++) {
             for (let j = 0; j<2; j++){
                 const hour = i.toString().padStart(2, "0")
                 const minute = (j * 30).toString().padStart(2, "0")
