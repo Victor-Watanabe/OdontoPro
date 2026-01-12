ddebbb393943e5e1133995d2d29d87b86f5f6310
Victor-Watanabe <watavh@hotmail.com>
2026-01-12 20:23:00 -0300

    OP - 21 realizando mascará para valor de serviço.

--- src/app/(panel)/dashboard/service/_components/dialog-services.tsx
+++ src/app/(panel)/dashboard/service/_components/dialog-services.tsx
@@ -1,7 +1,7 @@
 "use cliente"
 
 import { DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog"
-import { useDialogServiceForm } from "./dialog-service-form"
+import { useDialogServiceForm, DialogServiceFormData } from "./dialog-service-form"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage
  } from "@/components/ui/form"
  import { Input } from "@/components/ui/input"
@@ -10,6 +10,24 @@ import { Button } from "@/components/ui/button"
 export function DialogService(){
     
     const form = useDialogServiceForm()
+
+    async function onSubmit(values : DialogServiceFormData){
+        console.log(values)
+    }
+
+    function changeCurrency(event : React.ChangeEvent<HTMLInputElement>){
+        let { value } = event.target
+        value = value.replace(/\D/g, "");
+        
+        if(value){
+            value = (parseInt(value, 10) / 100).toFixed(2);
+            value = value.replace('.',",")
+            value = value.replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+        }
+
+        event.target.value = value
+        form.setValue("price", value)
+    }
 
     return(
         <>
@@ -21,7 +39,8 @@ export function DialogService(){
             </DialogHeader>
 
             <Form {...form}>
-                <form className="space-y-2">
+                <form className="space-y-2"
+                onSubmit={form.handleSubmit(onSubmit)}>
                     <div className="flex flex-col">
                         <FormField
                         control={form.control}
@@ -49,7 +68,8 @@ export function DialogService(){
                                     Valor do Serviço:
                                 </FormLabel>
                                 <FormControl>
-                                    <Input {...field} placeholder="Ex: 120,00"/>
+                                    <Input {...field} placeholder="Ex: 120,00"
+                                    onChange={changeCurrency}/>
                                 </FormControl>
                                 <FormMessage/>
                             </FormItem>
