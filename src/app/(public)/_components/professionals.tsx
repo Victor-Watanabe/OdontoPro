db6b5e7d7b4918dc636c6ed17f4a81d69cbd37fb
Victor-Watanabe <watavh@hotmail.com>
2026-02-04 21:49:12 -0300

    OP - 35. Ajustando e configurando TimeZone do computador.

--- src/app/(public)/_components/professionals.tsx
+++ src/app/(public)/_components/professionals.tsx
@@ -1,13 +1,15 @@
 import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
-import OdontoCompany from "../../../../public/odontocompany.jpg";
-import RealceOdonto from "../../../../public/realceodonto.jpg";
-import Sorridents from "../../../../public/sorridents.jpg";
-import TheSmile from "../../../../public/thesmile.jpg";
+import fotoImg from "../../../../public/foto1.png";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
+import { User } from "@/generated/prisma/client";
 
-export function Professionals() {
+interface ProfessionalsProps{
+  professionals: User[]
+}
+
+export function Professionals({professionals} : ProfessionalsProps) {
   return (
     <section className="bg-gray-100">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-16">
@@ -16,12 +18,14 @@ export function Professionals() {
         </h2>
 
         <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
-          <Card className="overflow-hidden p-0">
+
+          {professionals.map((clinic)=>(
+            <Card className="overflow-hidden hover:shadow-lg duration-300" key={clinic.id}>
             <CardContent className="p-0">
               {/* IMAGEM */}
-              <div className="relative h-48 w-full">
+              <div className="relative h-50 w-full">
                 <Image
-                  src={OdontoCompany}
+                  src={clinic.image ?? fotoImg}
                   alt="Foto da Clínica"
                   fill
                   className="object-cover"
@@ -34,17 +38,18 @@ export function Professionals() {
                 <div className="flex items-start justify-between gap-2">
                   <div>
                     <h3 className="font-semibold leading-tight">
-                      Clínica OdontoCompany
+                      {clinic.name}
                     </h3>
                     <p className="text-sm text-gray-500">
-                      Av. Nossa Sra. de Fátima, n° 548 - Caiçara, Praia Grande - SP
+                      {clinic.address ?? "Endereço não informado."}
                     </p>
                   </div>
                   <div className="mt-1 w-2.5 h-2.5 rounded-full bg-emerald-500 shrink-0" />
                 </div>
 
                 <Link
-                  href="/Clinica123"
+                  target="_blank"
+                  href={`/clinica/${clinic.id}`}
                   className="w-full bg-emerald-500 hover:bg-emerald-400 text-white flex items-center justify-center py-2 rounded-md text-sm md:text-base font-medium transition-colors"
                 >
                   Agendar Horário
@@ -53,119 +58,8 @@ export function Professionals() {
               </div>
             </CardContent>
           </Card>
+          ))}
 
-          <Card className="overflow-hidden p-0">
-            <CardContent className="p-0">
-              {/* IMAGEM */}
-              <div className="relative h-48 w-full">
-                <Image
-                  src={Sorridents}
-                  alt="Foto da Clínica"
-                  fill
-                  className="object-cover"
-                  priority
-                />
-              </div>
-
-              {/* CONTEÚDO */}
-              <div className="p-4 space-y-4">
-                <div className="flex items-start justify-between gap-2">
-                  <div>
-                    <h3 className="font-semibold leading-tight">
-                      Clinica Sorridents Praia Grande
-                    </h3>
-                    <p className="text-sm text-gray-500">
-                      Av. Presidente Costa e Silva, n°1164 - Boqueirão, Praia Grande - SP
-                    </p>
-                  </div>
-                  <div className="mt-1 w-2.5 h-2.5 rounded-full bg-emerald-500 shrink-0" />
-                </div>
-
-                <Link
-                  href="/Clinica123"
-                  className="w-full bg-emerald-500 hover:bg-emerald-400 text-white flex items-center justify-center py-2 rounded-md text-sm md:text-base font-medium transition-colors"
-                >
-                  Agendar Horário
-                  <ArrowRight className="ml-2 w-4 h-4" />
-                </Link>
-              </div>
-            </CardContent>
-          </Card>
-
-          <Card className="overflow-hidden p-0">
-            <CardContent className="p-0">
-              {/* IMAGEM */}
-              <div className="relative h-48 w-full">
-                <Image
-                  src={TheSmile}
-                  alt="Foto da Clínica"
-                  fill
-                  className="object-cover"
-                  priority
-                />
-              </div>
-
-              {/* CONTEÚDO */}
-              <div className="p-4 space-y-4">
-                <div className="flex items-start justify-between gap-2">
-                  <div>
-                    <h3 className="font-semibold leading-tight">
-                      Clínica The Smile
-                    </h3>
-                    <p className="text-sm text-gray-500">
-                      Rua: Fumio Miyazi, n° 515 - Boqueirão, Praia Grande - SP
-                    </p>
-                  </div>
-                  <div className="mt-1 w-2.5 h-2.5 rounded-full bg-emerald-500 shrink-0" />
-                </div>
-
-                <Link
-                  href="/Clinica123"
-                  className="w-full bg-emerald-500 hover:bg-emerald-400 text-white flex items-center justify-center py-2 rounded-md text-sm md:text-base font-medium transition-colors"
-                >
-                  Agendar Horário
-                  <ArrowRight className="ml-2 w-4 h-4" />
-                </Link>
-              </div>
-            </CardContent>
-          </Card>
-          <Card className="overflow-hidden p-0">
-            <CardContent className="p-0">
-              {/* IMAGEM */}
-              <div className="relative h-48 w-full">
-                <Image
-                  src={RealceOdonto}
-                  alt="Foto da Clínica"
-                  fill
-                  className="object-cover"
-                  priority
-                />
-              </div>
-
-              {/* CONTEÚDO */}
-              <div className="p-4 space-y-4">
-                <div className="flex items-start justify-between gap-2">
-                  <div>
-                    <h3 className="font-semibold leading-tight">
-                      Clínica Realceodonto
-                    </h3>
-                    <p className="text-sm text-gray-500">
-                      Av. Presidente Kennedy, n° 6576 - Ocian, Praia Grande - SP
-                    </p>
-                  </div>
-                  <div className="mt-1 w-2.5 h-2.5 rounded-full bg-emerald-500 shrink-0" />
-                </div>
-
-                <Link
-                  href="/Clinica123"
-                  className="w-full bg-emerald-500 hover:bg-emerald-400 text-white flex items-center justify-center py-2 rounded-md text-sm md:text-base font-medium transition-colors"
-                >
-                  Agendar Horário
-                  <ArrowRight className="ml-2 w-4 h-4" />
-                </Link>
-              </div>
-            </CardContent>
-          </Card>
         </section>
       </div>
     </section>
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,14 +2,20 @@ import Footer from "./_components/Footer"
 import { Header } from "./_components/Header"
 import {Hero } from "./_components/hero"
 import { Professionals } from "./_components/professionals"
+import { getProfessionals } from "./_data-access/get-professionals"
 
-export default function home(){
+export default async function home(){
+  
+  const professionals = await getProfessionals();
+
+  
+
   return(
     <div className="flex flex-col min-h-screen">
       <Header></Header>
       <div>
           <Hero/>
-          <Professionals/>
+          <Professionals professionals={professionals || []}/>
       </div>
       <Footer/>
     </div>
